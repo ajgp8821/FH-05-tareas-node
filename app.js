21995d22f06eb239e69d90fbca1c6422e6365f20
ajgp8821 <ajgp8821@gmail.com>
2021-10-21 09:33:10 -0300

    Repetir menú de forma infinita

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 require('colors');
+const { showMenu, pause } = require('./helpers/messages');
 
 console.clear();
 
 const main = async() => {
   
   console.log('Hola mundo');
-  
+
+  let opt = '';
+
+  do{
+    opt = await showMenu();
+    console.log({opt});
+    if (opt !== '0') await pause();
+
+  } while (opt != '0');
+
 }
 
 main();
